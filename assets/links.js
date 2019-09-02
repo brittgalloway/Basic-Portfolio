ab2689e4798d9cccaf48b5c3ed46081cfdfc44f0
BrittGall <crlnfllr@gmail.com>
2019-09-01 23:24:03 -0500

    made only 1 .ready()

--- assets/links.js
+++ assets/links.js
@@ -2,18 +2,16 @@ $(document).ready(function() {
   $(".Hangman").wrap(
     "<a href='https://brittgalloway.github.io/WordGuessGame/' </a>"
   );
-});
-$(document).ready(function() {
+
   $(".rpg").wrap("<a href='https://brittgalloway.github.io/unit-4-game/' </a>");
-});
-$(document).ready(function() {
+
   $(".trivia").wrap(
     "<a href='https://brittgalloway.github.io/TriviaGame/' </a>"
   );
 });
-//   $(document).ready(function() {
+//
 //     $(".rutgers").wrap("<a href='https://brittgalloway.github.io/unit-4-game/' </a>");
 //   });
-//   $(document).ready(function() {
+//
 //     $(".rock").wrap("<a href='https://brittgalloway.github.io/unit-4-game/' </a>");
 //   });
