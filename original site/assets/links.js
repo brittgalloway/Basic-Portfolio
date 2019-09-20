8a97f10ea90df447c296a24983dbd519bd13a159
BrittGall <crlnfllr@gmail.com>
2019-09-20 00:51:13 -0500

    moving files in folders, coping files

---
+++ original site/assets/links.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".Hangman").wrap(
+    "<a href='https://brittgalloway.github.io/WordGuessGame/' </a>"
+  );
+
+  $(".rpg").wrap("<a href='https://brittgalloway.github.io/unit-4-game/' </a>");
+
+  $(".trivia").wrap(
+    "<a href='https://brittgalloway.github.io/TriviaGame/' </a>"
+  );
+
+  $(".rutgers").wrap(
+    "<a href='https://brittgalloway.github.io/GifTastic' </a>"
+  );
+
+  $(".rock").wrap(
+    "<a href='https://brittgalloway.github.io/TrainScheduler/' </a>"
+  );
+});
